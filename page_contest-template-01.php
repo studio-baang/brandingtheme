e2a843d68cb6693e17de0b9031742bbe504faffe
baang <qkddpwns@gmail.com>
2022-08-09 00:00:08 +0900

    0.0.1 wordpress webhook test 5

--- page_contest-template-01.php
+++ page_contest-template-01.php
@@ -3,7 +3,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header class="header">
-<h1 class="entry-title" itemprop="name"><strong><?php the_title(); ?></strong><br>logo design wordpress test 4</h1> <?php edit_post_link(); ?>
+<h1 class="entry-title" itemprop="name"><strong><?php the_title(); ?></strong><br>logo design wordpress test 5</h1> <?php edit_post_link(); ?>
 </header>
 <div class="entry-content" itemprop="mainContentOfPage">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
