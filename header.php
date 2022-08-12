50203e55c2b064d50588794e302be21701080374
studio-baang <qkddpwns@gmail.com>
2022-08-13 01:02:00 +0900

    구글 애널리틱스 태그 추가

--- header.php
+++ header.php
@@ -4,6 +4,15 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3YRBY8WDL9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3YRBY8WDL9');
+</script>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
