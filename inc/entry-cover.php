50a85ab4adf5a1c5b64335dd1673a6b49b2fddcf
studio-baang <qkddpwns@gmail.com>
2022-09-28 16:51:08 +0900

    cover-block 수정 및 cmoponunt 수정

--- inc/entry-cover.php
+++ inc/entry-cover.php
@@ -1,12 +1,13 @@
 <?php $fields = get_fields();  ?>
 <section class="studio-baang-cover">
-    <?php
-    if($fields["background-color"]) : ?>
-    <span aria-hidden="true" class="studio-baang-cover__background-color <?php if($fields["background"]) { echo 'studio-baang-cover__background-color--with-image'; }; ?>" style="background-color: <?=$fields["background-color"]?>"></span>
-    <?php endif; ?>
         
     <?php if($fields["background"]) : ?>
     <img  aria-hidden="true"  class="studio-baang-cover__image studio-baang-cover__image--cover" src="<?=$fields["background"]["url"]?>" alt="">
+    <?php endif; ?>
+
+        <?php
+    if($fields["background-color"]) : ?>
+    <span aria-hidden="true" class="studio-baang-cover__background-color <?php if($fields["background"]) { echo 'studio-baang-cover__background-color--with-image'; }; ?>" style="background-color: <?=$fields["background-color"]?>"></span>
     <?php endif; ?>
     
     <?php if($fields["logo-image"]) : ?>
