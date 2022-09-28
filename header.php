d1c18b33e4dbb8880d15d62668fc81ef7fb0e372
studio-baang <qkddpwns@gmail.com>
2022-09-28 15:38:09 +0900

    1.1.0 개선
    갤러리 블록 size large로 고정하였음
    갤러리 블록 full 선택 시 container 사이즈가 되도록 수정하였음

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
   gtag('config', 'G-3YRBY8WDL9');
 </script>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1, minumum-sacle=1.0, maximum-scale=1.0, user-scalable=no" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- inc/patterns/gallery.php
+++ inc/patterns/gallery.php
@@ -4,7 +4,7 @@ return array(
             'categories' => array('studio-baang'),
             'blockTypes' => array( 'core/gallery' ),
             'content'     => '<!-- wp:group -->
-                            <div class="wp-block-group"><!-- wp:gallery {"columns":1,"imageCrop":false,"linkTo":"none","sizeSlug":"full","className":"studio-baang-gallery"} -->
+                            <div class="wp-block-group"><!-- wp:gallery {"columns":1,"imageCrop":false,"linkTo":"none","sizeSlug":"large","className":"studio-baang-gallery"} -->
                             <figure class="wp-block-gallery has-nested-images columns-1 studio-baang-gallery"></figure>
                             <!-- /wp:gallery --></div>
                             <!-- /wp:group -->',
